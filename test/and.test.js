4527187c0f045725a625db2ca2863b2e024b7626
Valen Varangu Booth <valen@valenvb.ca>
2019-04-03 14:10:00 -0400

    Testing for And

--- test/and.test.js
+++ test/and.test.js
@@ -1,26 +1,23 @@
 let And = require('../src/and')
 let Set = require('../src/set')
-const set1 = new Set([-1,0], [0,1], [1,0])
-const set2 = new Set([0,0], [1,1], [2,0])
+const set1 = new Set("test1", [-1,0], [0,1], [1,0])
+const set2 = new Set("test2", [0,0], [1,1], [2,0])
 
 describe('And', ()=>{
     it("exists", ()=>{
         expect(And)
     })
 
-    it('requires two sets as input',()=>{
-        expect(And).toThrowError()
+    it('is an object with methods', ()=>{
+        expect(And.drastic).toBeInstanceOf(Function);
+        expect(And.einstein).toBeInstanceOf(Function);
+        expect(And.godel).toBeInstanceOf(Function);
+        expect(And.goguen).toBeInstanceOf(Function);
+        expect(And.hamaches).toBeInstanceOf(Function);
+        expect(And.luka).toBeInstanceOf(Function);
+        expect(And.nilpotent).toBeInstanceOf(Function);
 
-        const f = ()=>{
-            And(set1, set2)
-        }
-        expect(f).not.toThrowError()
     })
 
-
-    it('returns a Set', ()=>{
-        const r = And(set1, set2, And.T_NORMS.GODEL)
-        expect(r).toBeInstanceOf(Set)
-    })
 
 })
