7cdb708ce2c74f3ab5d665574821646a608d3cf7
Valen Varangu Booth <valen@valenvb.ca>
2019-04-03 14:11:40 -0400

    Or functions

---
+++ src/or.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Or = {
+    godel : function(a, b){
+        return a>b? a : b //max
+    },
+    goguen : function(a, b){
+        return a+b - a*b
+    },
+    luka : function(a, b){
+        return 
+    },
+    hamaches : function(a, b){
+        return 
+    },
+    einstein : function(a, b){
+        return 
+    },
+    nilpotent : function(a, b){
+       
+    },
+    drastic : function(a, b){
+        
+    }
+
+}
+
+
+module.exports = Or
