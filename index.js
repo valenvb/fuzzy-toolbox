4dfc4cde59fd7007969d23e62d87d78095c7ff6f
Valen Varangu Booth <valen@valenvb.ca>
2019-04-03 14:08:35 -0400

    export all the classes

---
+++ index.js
@@ -0,0 +1,9 @@
+const Fuzzy = {
+    Set : require('./src/set'),
+    And : require('./src/and'),
+    Or  : require('./src/or'),
+    Inference : require('./src/inference')
+
+}
+
+module.exports = Fuzzy
